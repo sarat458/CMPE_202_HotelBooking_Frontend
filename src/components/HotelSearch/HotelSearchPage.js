082e578a4980e6f83c90f9ba59660a2827705482
Gali Saketh <sakethgali@gmail.com>
2022-05-07 11:18:56 -0700

    Folder structure

---
+++ src/components/HotelSearch/HotelSearchPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class HotelSearchPage extends Component {
+  render() {
+    return (
+      <div>HotelSearchPage</div>
+    )
+  }
+}
+
+export default HotelSearchPage;
